0c5f258e460b7a9328c24d642e5607b628fb6f98
steevy <steevesanon6@gmail.com>
2023-02-12 20:42:00 -0500

    Initial Project Add

---
+++ sfg-pet-clinic_one/src/main/java/steevelinformaticien/com/sfgpetclinic_one/SfgPetClinicOneApplication.java
@@ -0,0 +1,13 @@
+package steevelinformaticien.com.sfgpetclinic_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicOneApplication.class, args);
+	}
+
+}
